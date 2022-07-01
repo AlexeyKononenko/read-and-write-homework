9ac5536c945b884e08d33418055c1f11d8089f0c
Алексей Кононенко <tetris.bf3@gmail.com>
2022-07-01 23:33:47 +0300

    второй комит (добалена функция сортировки в 3 задании)

--- zd3.py
+++ zd3.py
@@ -2,15 +2,20 @@ import os
 from pprint import pprint
 path = r'\read and write homework\test' 
 def reader():
+    all_data = {}
+    data = {}
     for filename in os.listdir(path):
         with open(os.path.join(path, filename), encoding='utf-8')as f:
             text = f.readlines()
-            data = dict({'name':filename, 'line':len(text), 'text':text})
-    return data   
+            data[filename] = (len(text), text)
+            all_data.update(data)
+    return all_data
 
-
-
-print(reader())    
+pprint(reader())    
     
-    
+def sorted_dict():
+    sorted_file = sorted(reader().items(), key=lambda x:x[1])
+    print(sorted_file)
+    return sorted_file   
          
+pprint(sorted_dict())
